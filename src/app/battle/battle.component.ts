3fee69fab02b7763a142ad33a9b28f9997c4ab81
FourmiPanda <ismail.el-khantache@etu.univ-nantes.fr>
2018-11-24 15:08:25 +0100

    Improve battle comp

--- src/app/battle/battle.component.ts
+++ src/app/battle/battle.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Howl, Howler} from 'howler';
 
 @Component({
   selector: 'app-battle',
@@ -9,16 +10,62 @@ export class BattleComponent implements OnInit {
 
   private audio;
 
+  enemy = {
+    name: 'Zekrom',
+    lvl: '100',
+    url: 'assets/images/Zekrom_XY.gif',
+    hp: '100'
+
+  };
+  ally = {
+    name: 'Rayquaza',
+    lvl: '100',
+    url: 'assets/images/Rayquaza-back_XY.gif',
+    hp: '100',
+    comp_1: '1',
+    comp_2: '2',
+    comp_3: '3',
+    comp_4: '4',
+  };
+  message = {
+    battle_text: '',
+    comp_1: 'FIGHT',
+    comp_2: 'BAG',
+    comp_3: 'POKéMON',
+    comp_4: 'RUN',
+  };
+
   constructor() { }
 
   ngOnInit() {
-    this.audio = new Audio();
-    this.audio.src = "assets/sound/battle.mp3";
-    this.audio.load();
-    this.audio.play();
+    console.log("BATTLE STARTED");
+    this.audio = new Howl({
+      src: ['assets/sound/battle.mp3']
+    });
+    this.start();
   }
 
-  playAudio(){
+  start(){
     this.audio.play();
+    this.message.battle_text = "The battle is starting !";
+    setTimeout(() => {
+      this.message.battle_text = "What will "+this.ally.name+" do ?";
+    }, 3000);
+  }
+
+  doComp1(){
+    console.log("1");
+  }
+
+  doComp2(){
+
+  }
+
+  doComp3(){
+
+  }
+
+  doComp4(){
+
   }
 }
--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Howl, Howler} from 'howler';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-start',
@@ -6,11 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./start.component.scss']
 })
 export class StartComponent implements OnInit {
+  title = 'Angumon';
+  private audio;
 
-  constructor() { }
+  constructor(public router: Router) { }
 
   ngOnInit() {
+    console.log("Hello trainer !");
+    this.audio = new Howl({
+      src: ['assets/sound/start.mp3']
+    });
+    this.audio.play();
+  }
 
+  start(){
+    this.audio.stop();
+    this.router.navigate(['/battle']);
   }
 
 }
